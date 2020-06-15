5b440a416ae9ab32490e030d2cd335da60b9c70c
agarwalvishu6300 <42807303+agarwalvishu6300@users.noreply.github.com>
2020-06-15 14:50:03 +0530

    from vishwas

--- server/models/product.js
+++ server/models/product.js
@@ -19,7 +19,7 @@ const productSchema = new mongoose.Schema(
       type: String,
       trim: true,
       required: true,
-      maxlength: 2000
+      maxlength: 200
     },
     price: {
       type: Number,
