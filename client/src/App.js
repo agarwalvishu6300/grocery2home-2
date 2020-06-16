2264fdb267e2fd6d82d6bbc48c50ae0e9819773a
agarwalvishu6300 <42807303+agarwalvishu6300@users.noreply.github.com>
2020-06-16 15:30:44 +0530

    sign in
    signin page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Particles from 'react-particles-js';
+import Signin from './signin/Signin'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+const particlesOptions ={
+               particles: {
+                    number: {
+                     value : 200,
+                     density: {
+                     enable: true,
+                     value_area: 800
+                              }
+                            }
+                          }
+                       }
+
+class App extends Component {
+
+
+
+render()
+ {
+   return (
+
+      <div className="App">
+        <Signin/>
+        <Particles className='particles'
+        params={particlesOptions}
+        />
+
+      </div>
+
+ );
+}
+
 }
 
 export default App;
